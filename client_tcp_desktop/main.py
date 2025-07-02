f0dcd9908c5642b032879639da48d2c4addce35a
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-07-02 22:48:04 +0300

    update

--- client_tcp_desktop/main.py
+++ client_tcp_desktop/main.py
@@ -2,9 +2,6 @@ from web import thread_app_flask_run
 from tcp import thread_run_tcp_client
 from desktop import thread_desktop_start
 
-from ..storage.tcp_channel_data import *
-
-# if __name__ == '__main__':
 thread_run_tcp_client()
 thread_app_flask_run()
 thread_desktop_start()
