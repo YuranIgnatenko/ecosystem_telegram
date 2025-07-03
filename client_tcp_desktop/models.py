3736693946ef67f0b29d9367e7598ad08e2b01a7
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-07-03 17:55:45 +0300

    add support template-base pag_data for all pages (safe any some values)

--- client_tcp_desktop/models.py
+++ client_tcp_desktop/models.py
@@ -1,18 +1,22 @@
 class ModelPlatformInfo:
-	def __init__(self, title, logo_url):
+	def __init__(self, title, logo_url, year, telegram_channel_url_news):
 		self.title = title 
 		self.logo_url = logo_url
+		self.year = year
+		self.telegram_channel_url_news = telegram_channel_url_news 
 
 
 class ModelUserData:
-	def __init__(self, login, email):
+	def __init__(self, login, email, logo_url):
 		self.login = login
 		self.email = email
 		self.messages = []
+		self.logo_url = logo_url
 
 
 class ModelPageData:
 	def __init__(self, platform_info, user_data):
 		self.platform_info = platform_info
 		self.user_data = user_data
+
 		
--- client_tcp_desktop/web.py
+++ client_tcp_desktop/web.py
@@ -10,50 +10,49 @@ page_data = ModelPageData(
 	platform_info=ModelPlatformInfo(
 		title="Platform Ecosystem",
 		logo_url="",
+		year=2025,
+		telegram_channel_url_news="http://t.me/news_platform_ecosystem"
+
 	),
 	user_data=ModelUserData(
-		login="admin",
-		email="admin@admin.ext"
+		login="admin 78320",
+		email="admin@admin.ext",
+		logo_url="static/img/notification/4.jpg"
 	)
 )
 
 app = Flask(__name__)
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
-
-@app.route('/')  
-def root():  
-	return render_template('analytics.html')  
-
+225
 @app.route('/404')  
 def p404():
-	value = "TEST STRING"
-	return render_template('404.html', value = value) 
+	return render_template('404.html', page_data = page_data) 
 
 @app.route('/500')  
 def p500():  
-	return render_template('500.html') 
+	return render_template('500.html', page_data = page_data) 
 
 @app.route('/blog')  
 def blog():  
-	return render_template('blog.html')
+	return render_template('blog.html', page_data = page_data)
 
 @app.route('/mailbox')  
 def mailbox():  
-	return render_template('mailbox.html')
+	return render_template('mailbox.html', page_data = page_data)
 
 @app.route('/login')
 def login(): 
-	return render_template('login.html')
+	return render_template('login.html', page_data = page_data)
 
 @app.route('/register')
 def register(): 
-	return render_template('register.html')
+	return render_template('register.html', page_data = page_data)
 
 
-@app.route('/account-detail')
+@app.route('/account')
 def account_detail(): 
-	return render_template('account.html')
+	return render_template('account.html', page_data = page_data)
 
 def thread_app_flask_run():
 	thread_app_flask = threading.Thread(target=app.run)
